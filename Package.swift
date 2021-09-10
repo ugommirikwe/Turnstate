6cf64e684872307c273dcf735e4128ed7f6f1aa3
Ugo Mmirikwe <ugommirikwe@gmail.com>
2021-09-10 19:12:03 +0200

    Deleted files not used.

--- Package.swift
+++ Package.swift
@@ -20,8 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ReduxSwift",
-            dependencies: [],
-            exclude: ["ReduxStoreProtocol.md"]),
+            dependencies: []),
         .testTarget(
             name: "ReduxSwiftTests",
             dependencies: ["ReduxSwift"]),
