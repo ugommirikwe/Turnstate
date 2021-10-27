c5811fc9cb39280c6d628b43068d872e69abae50
Ugo Mmirikwe <ugommirikwe@gmail.com>
2021-10-27 18:43:06 +0200

    Add Copyright Text In Files

--- Sources/Turnstate/StoreActionProtocol.swift
+++ Sources/Turnstate/StoreActionProtocol.swift
@@ -1,3 +1,19 @@
+//
+//  Copyright (c) 2021. Ugo Mmirikwe
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
 import Foundation
 
 /// Defines a base type for actions that can be dispatched in an instance of a [store](x-source-tag://Store). An action represents an object describing a change, alongside any associated piece of data (usually referred to as its payload), that makes sense for your application. Actions are the only way to get data into the store, so any data, whether from UI events, network callbacks, or other sources such as WebSockets needs to eventually be dispatched as actions.
